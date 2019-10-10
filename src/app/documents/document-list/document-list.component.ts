4b4b18985097a09c3d36698d507e087397a60864
Fritz Fevrier <fev17001@byui.edu>
2019-10-10 00:46:00 -0600

    add new files

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Document } from '../document.model';
 @Component({
   selector: 'app-document-list',
   templateUrl: './document-list.component.html',
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  
+  documents: Document[] = [
+    new Document('1', 'CIT 260 -', 'Object Oriented Programing', 'https://content.byui.edu/fileb7c3e5ed-6947-497f-9d32-4e5b397cac/1/CIT 260 course description.pdf', null),
+    new Document('2', 'CIT 425 -', 'Data Warehouse', 'https://content.byui.edu/fileb7c3e5ed-6947-497f-9d32-4e5b397cac/1/CIT 425 course description.pdf', null),
+    new Document('3', 'CIT 366 -', 'Full Web Stack Development', 'https://content.byui.edu/fileb7c3e5ed-6947-497f-9d32-4e5b397cac/1/CIT 366 course description.pdf', null),
+    new Document('4', 'CIT 460 -', 'Enterprise Development', 'https://content.byui.edu/fileb7c3e5ed-6947-497f-9d32-4e5b397cac/1/CIT 460 course description.pdf', null),
 
+  ];
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+ }
 }
